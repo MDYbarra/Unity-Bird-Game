801e6359cc9c07b7bb560b5dd4db82af07678670
MDYbarra <mybarr3@uic.edu>
2020-03-06 21:12:18 -0600

    Ring Disappears when bird flys throw

---
+++ Assets/Scripts/RingHole.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingHole : MonoBehaviour
+{
+    // public Score scoreManager;
+    public AudioSource pop;
+    public GameObject ring, ringTarget;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //scoreManager.AddPoint();
+        Debug.Log("Collision Detected");
+        var hit = collision.gameObject;
+        //var hitObject = hit.GetComponent<Bullet>();
+        //if (hitObject != null)
+        //{
+        //    pop.Play();
+        //    Destroy(gameObject);// make sure hit object is bullet, if so destroy this target.
+        //    return;
+
+        //}
+        var hitObject2 = hit.GetComponent<CharacterController>();
+        if (hitObject2 != null)
+        {
+            ringTarget.SetActive(false);
+            ring.SetActive(false);
+            pop.Play();
+            Destroy(gameObject);// make sure hit object is bullet, if so destroy this target.
+            return;
+        }
+        //Destroy(collision.gameObject);
+    }
+}
